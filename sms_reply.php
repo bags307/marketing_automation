90923674e68b1a9021630a189c5f5d220ea452ef
root <root@marketing.buildabigidea.com>
2016-08-09 16:07:59 +0000

    init

---
+++ sms_reply.php
@@ -0,0 +1,7 @@
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Message>Thanks for responding, but this message from Big Idea Marketing doesn't allow for replies!</Message>
+</Response>
